7fb994cd5808ea0e9f0b32419d66c76b2520070e
Dayan Yamin <yamindayan@gmail.com>
2022-07-20 21:00:52 -0400

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './containers/Home/index';
+import Home from './pages/Home/index';
 
 function App() {
   return (
