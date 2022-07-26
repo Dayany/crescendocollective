6d76444a7c86ca4924b3252c70019e67ba5f6cb5
Dayan Yamin <yamindayan@gmail.com>
2022-07-26 12:44:53 -0400

    Show Special on ingredient

--- src/components/Ingredients/CardIngredient.js
+++ src/components/Ingredients/CardIngredient.js
@@ -1,6 +1,16 @@
 import { Card, CardContent, Typography } from '@mui/material';
+import { useMemo, useState } from 'react';
 
-function CardIngredient({ ingredient }) {
+function CardIngredient({ ingredient, specials }) {
+  const [specialInfo, setSpecialInfo] = useState(null);
+  useMemo(() => {
+    specials?.find((element) => {
+      if (element.ingredientId === ingredient.uuid) {
+        setSpecialInfo(element);
+      }
+    });
+  }, [specials, ingredient.uuid]);
+
   return (
     <Card sx={{ marginBottom: 2, minWidth: 275 }}>
       <CardContent>
@@ -9,6 +19,9 @@ function CardIngredient({ ingredient }) {
         </Typography>
         <Typography variant="body2"> Amount: {ingredient.amount}</Typography>
         <Typography variant="body2"> Measurement: {ingredient.measurement}</Typography>
+        {specialInfo ? (
+          <Typography variant="body2"> SPECIAL: {specialInfo.title}</Typography>
+        ) : null}
       </CardContent>
     </Card>
   );
--- src/components/Ingredients/ShowIngredients.js
+++ src/components/Ingredients/ShowIngredients.js
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react';
+import axiosAPI from '../../lib/axios/API';
 import CardIngredient from './CardIngredient';
 
 function ShowIngredient({ ingredients }) {
+  const [specials, setSpecials] = useState([]);
+  useEffect(() => {
+    axiosAPI
+      .get(`/specials`)
+      .then((res) => {
+        setSpecials(res.data || []);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <h2>Ingredients</h2>
       {ingredients &&
         ingredients.map((ingredient, index) => (
-          <CardIngredient key={ingredient.uuid} ingredient={ingredient} index={index} />
+          <CardIngredient
+            key={ingredient.uuid}
+            ingredient={ingredient}
+            index={index}
+            specials={specials}
+          />
         ))}
     </>
   );
